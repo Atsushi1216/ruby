273f2277f72cbd8e189043a570210231f915a710
Atsushi1216 <ats1216@gmail.com>
2022-03-07 04:26:45 +0000

    [Add]lesson 5..7-1

---
+++ lesson5.rb
@@ -0,0 +1,3 @@
+# array 配列の確認
+subjects = ["国語","数学","理科","社会"]
+puts subjects[2]
---
+++ lesson6.rb
@@ -0,0 +1,9 @@
+# 条件分岐の確認
+total_price = 20
+if total_price > 100
+  puts "みかんを購入。所持金に余りあり"
+elsif total_price == 100
+  puts "みかんを購入。所持金は0円"
+else
+ puts "みかんを購入することができません。"
+end
